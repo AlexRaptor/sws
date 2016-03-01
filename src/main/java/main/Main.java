16455d561c294581b9b37048206e59eaaad233f0
raptor <raptor@developmentontheedge.com>
2016-03-01 10:52:52 +0600

    home work #1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,20 +4,25 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestServlet;
+import servlets.MirrorRequestServlet;
 
 public class Main
 {
     public static void main( String[] args ) throws Exception
     {
-
-        AllRequestServlet allRequestServlet = new AllRequestServlet();
-
         Server server = new Server( 8080 );
         ServletContextHandler context = new ServletContextHandler( ServletContextHandler.SESSIONS );
         server.setHandler( context );
+        AllRequestServlet allRequestServlet = new AllRequestServlet();
         context.addServlet( new ServletHolder( allRequestServlet ), "/*" );
 
+        MirrorRequestServlet mirrorRequestServlet = new MirrorRequestServlet();
+        context.addServlet( new ServletHolder( mirrorRequestServlet ), "/mirror" );
+
         server.start();
+
+        System.out.println( "Server started" );
+
         server.join();
     }
 }
---
+++ src/main/java/servlets/MirrorRequestServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MirrorRequestServlet extends HttpServlet
+{
+    @Override
+    protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
+    {
+
+        String keyParam = req.getParameter( "key" );
+
+        resp.getWriter().println( keyParam );
+
+        resp.setContentType( "text/html;charset=utf-8" );
+        resp.setStatus( HttpServletResponse.SC_OK );
+    }
+}
