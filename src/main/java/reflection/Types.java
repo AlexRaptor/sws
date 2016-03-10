560c5bbd1b59e7cfad9f2e30ac4d227d605d21cd
raptor <raptor@developmentontheedge.com>
2016-03-10 18:50:45 +0600

    refactor and update ReflectionHelper

---
+++ src/main/java/reflection/Types.java
@@ -0,0 +1,22 @@
+package reflection;
+
+/**
+ * @author raptor
+ *         date: 10.03.16
+ */
+public enum Types {
+    BYTE,
+    BOOLEAN,
+    SHORT,
+    CHAR,
+    INT,
+    FLOAT,
+    LONG,
+    DOUBLE,
+    STRING;
+
+    public static Types getType(Class<?> clazz) {
+        String className = clazz.getSimpleName().toUpperCase();
+        return Types.valueOf(className);
+    }
+}
